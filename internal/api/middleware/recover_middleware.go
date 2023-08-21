41fa030cd915013430b9d295d7854ad091ca2d1d
Jamal5p <jamal5p4411244@gmail.com>
2023-08-21 12:05:12 +0100

    small change

--- internal/api/middleware/recover_middleware.go
+++ internal/api/middleware/recover_middleware.go
@@ -11,9 +11,7 @@ func RecoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
-				w.Header().Set("Connection", "close")
 				customerrors.ServerErrorResponse(w, r, fmt.Errorf("%s", err))
-
 			}
 		}()
 
